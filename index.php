5012ae921d1b132c9a7c2d39a6ed87225d9208c8
Ricardo318 <97570350+Ricardo318@users.noreply.github.com>
2022-01-14 12:42:02 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+require"/restaurant/controller/pedidoControllers.php"
+
+$pedidoControllers= new PedidoController();
+
+$validacion = (isset($_GET['op']) && !empty($_GET['op']));
+
+if(!$validacion){
+    $pedidControllers->inicio();
+}else{
+
+    $op=$_GET["op"];
+
+    if($op=="listar"){
+        $pedidoControllers->listar();
+    }
+
+    if($op=="agregar"){
+        $pedidoControllers->agregar();
+    }
+
+    if($op=="agregarPaciente"){
+        $mesa = $_POST['mesa'];
+        $descripcion = $_POST['txtdescripcion'];
+        
+        $pedidoNuevo = new Pedido();
+        
+        $pacienteNuevo->setRut($mesa);
+        $pacienteNuevo->setNombre($descripcion); 
+
+        $pedidoControllers->agregarPedido($pedidoNuevo);
+    }
+
+    if($op=="inicio"){
+        $pacienteController->inicio();
+    }
+
+    if($op=="eliminar"){
+        $rut = $_GET['rut'];
+        $pacienteController->eliminar($rut);
+    }
+}
+
+
+
+
+
+?>
